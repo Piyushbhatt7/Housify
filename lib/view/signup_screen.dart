2b5bacb41ebfddb44e7bb77fdcdcad43ea2ff6da
vengersmoon <vengersmon003@gmail.com>
2025-01-22 11:04:06 +0530

    g

--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -8,10 +8,18 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  TextEditingController _emailTextEditingController = TextEditingController();
+  TextEditingController _passwordTextEditingController = TextEditingController();
+  TextEditingController _firstNameTextEditingController = TextEditingController();
+  TextEditingController _lastNameTextEditingController = TextEditingController();
+  TextEditingController _cityTextEditingController = TextEditingController();
+  TextEditingController _countryTextEditingController = TextEditingController();
+  TextEditingController _bioTextEditingController = TextEditingController();
+  
+  final _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return Scaffold();
   }
 }
