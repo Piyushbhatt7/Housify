fb1ba232f5f4867eed31f0892067fae3f219ef40
vengersmoon <vengersmon003@gmail.com>
2025-01-22 22:55:54 +0530

    d

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -16,7 +16,6 @@ class _LoginScreenState extends State<LoginScreen> {
  TextEditingController _emailTextEditingController = TextEditingController();
  TextEditingController _passwordTextEditingController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
