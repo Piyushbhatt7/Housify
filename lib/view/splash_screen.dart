27856de320e4c05b6555506349315d3dd5e8cc7d
vengersmoon <vengersmon003@gmail.com>
2025-01-22 10:27:40 +0530

    f

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
 
-    Timer(const Duration(seconds: 2), (){
+    Timer(const Duration(seconds: 3), (){
     Get.to(LoginScreen());
 
     });
