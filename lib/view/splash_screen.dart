ffe114abbd7835bbd78c37226f2206e2d7e28256
vengersmoon <vengersmon003@gmail.com>
2025-01-22 10:19:10 +0530

    t

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -17,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     Timer(const Duration(seconds: 2), (){
-
+    Get.to(page)
 
     });
   }
