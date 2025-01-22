f44439e4520224e537e87edbf8db8c89ac799005
vengersmoon <vengersmon003@gmail.com>
2025-01-22 15:50:13 +0530

    k

--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -9,34 +9,56 @@ class SignupScreen extends StatefulWidget {
 
 class _SignupScreenState extends State<SignupScreen> {
   TextEditingController _emailTextEditingController = TextEditingController();
-  TextEditingController _passwordTextEditingController = TextEditingController();
-  TextEditingController _firstNameTextEditingController = TextEditingController();
-  TextEditingController _lastNameTextEditingController = TextEditingController();
+  TextEditingController _passwordTextEditingController =
+      TextEditingController();
+  TextEditingController _firstNameTextEditingController =
+      TextEditingController();
+  TextEditingController _lastNameTextEditingController =
+      TextEditingController();
   TextEditingController _cityTextEditingController = TextEditingController();
   TextEditingController _countryTextEditingController = TextEditingController();
   TextEditingController _bioTextEditingController = TextEditingController();
-  
+
   final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-         flexibleSpace: Container(
+      appBar: AppBar(
+        flexibleSpace: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(colors: 
-            [
+              gradient: LinearGradient(
+            colors: [
               Colors.pinkAccent,
               Colors.amberAccent,
             ],
-              begin: FractionalOffset(0.0, 0.0),
-              end: FractionalOffset(1.0, 0.0),
-              stops: [0.0, 1.0],
-              tileMode: TileMode.clamp,
-            )
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          )),
+        ),
+        title: Text(
+          "Create New Account",
+          style: TextStyle(color: Colors.white,
+          
           ),
-         ),
-       ),
+        ),
+      ),
+
+      body: Container(
+         decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Colors.pinkAccent,
+            Colors.amberAccent,
+          ],
+          begin: FractionalOffset(0, 0),
+          end: FractionalOffset(1, 0),
+          stops: [0, 1],
+          tileMode: TileMode.clamp,
+        )),
+      ),
     );
   }
 }
