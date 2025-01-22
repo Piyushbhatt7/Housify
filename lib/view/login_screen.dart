306a2242d08b0e06d9db1bb6bd9acec9410a7751
vengersmoon <vengersmon003@gmail.com>
2025-01-22 10:22:02 +0530

    d

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -10,6 +10,23 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Colors.pinkAccent,
+            Colors.amberAccent,
+          ],
+          begin: FractionalOffset(0, 0),
+          end: FractionalOffset(1, 0),
+          stops: [0, 1],
+          tileMode: TileMode.clamp,
+        )),
+        ),
+      ),
+    );
   }
 }
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:housify/view/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     Timer(const Duration(seconds: 2), (){
-    Get.to(page)
+    Get.to(LoginScreen());
 
     });
   }
