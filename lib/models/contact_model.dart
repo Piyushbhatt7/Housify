5500e9f3d6cb074ff7c6ac6b424a130fcb60f156
vengersmoon <vengersmon003@gmail.com>
2025-01-23 21:16:03 +0530

    g

---
+++ lib/models/contact_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ContactModel {
+   
+   String? id;
+   String? firstName;
+   String? lastName;
+   String? fullName;
+   MemoryImage? displayImage;
+   
+}
