f14248922afcf16c3a49f94344b6f9f12f9ec304
vengersmoon <vengersmon003@gmail.com>
2025-01-23 21:08:03 +0530

    g

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,8 +1,11 @@
 class UserModel {
     
     String? email;
+    String? password;
     String? bio;
     String? city;
     String? country;
+    String?  isHost;
+    String? isCurrentlyHosting;
     
 }
