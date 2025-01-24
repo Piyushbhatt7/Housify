82f452062220ecd411acf25e767443b20b3a9536
vengersmoon <vengersmon003@gmail.com>
2025-01-24 12:02:25 +0530

    g

---
+++ lib/view_model/user_view_model.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserViewModel {
+   
+    Future<void> saveUserToFirestore (bio, city, country, email, firstName, lastName, id) async 
+      {
+        Map<String, dynamic> dataMap = 
+         {
+            "bio": bio,
+            "city": city,
+            "country": country,
+            "email": email,
+            "firstName": firstName,
+            "lastName": lastName,
+            "isHost": false,
+            "myPostingIds": [],
+            "savedPostingIDs": [],
+            "earnings": 0,
+         };
+
+         await FirebaseFirestore.instance.collection("users").doc(id).set(dataMap);
+      }
+}
