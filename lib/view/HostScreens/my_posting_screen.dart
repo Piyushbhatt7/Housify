e44b0e2cf43e66c412e128f75b5f72934ddfb6e7
vengersmoon <vengersmon003@gmail.com>
2025-01-27 22:03:32 +0530

    g

---
+++ lib/view/HostScreens/my_posting_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyPostingScreen extends StatefulWidget {
+  const MyPostingScreen({super.key});
+
+  @override
+  State<MyPostingScreen> createState() => _MyPostingScreenState();
+}
+
+class _MyPostingScreenState extends State<MyPostingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
