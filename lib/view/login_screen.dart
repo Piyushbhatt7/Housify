4803bd282fc73f5966eefdff31ed6eb0c3f29257
vengersmoon <vengersmon003@gmail.com>
2025-01-27 11:33:24 +0530

    f

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -98,7 +98,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                 }
                             
                                     return null; 
-                              },
+                              }, 
                             ),
                     
                           ),
