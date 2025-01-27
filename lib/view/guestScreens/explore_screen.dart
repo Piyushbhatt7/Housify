a3928e238b586d8cb68065ed9d7dec5f6fb44c61
vengersmoon <vengersmon003@gmail.com>
2025-01-27 11:40:51 +0530

    r

--- lib/view/guestScreens/explore_screen.dart
+++ lib/view/guestScreens/explore_screen.dart
@@ -1,11 +1,13 @@
-class name extends StatefulWidget {
-  const name({super.key});
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({super.key});
 
   @override
-  State<name> createState() => _nameState();
+  State<ExploreScreen> createState() => _ExploreScreenState();
 }
 
-class _nameState extends State<name> {
+class _ExploreScreenState extends State<ExploreScreen> {
   @override
   Widget build(BuildContext context) {
     return Container();
