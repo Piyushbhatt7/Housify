7adfb96e0edd7120dee7f5b8f522a03a8f771e4b
vengersmoon <vengersmon003@gmail.com>
2025-01-27 11:37:45 +0530

    f

--- lib/view/guest_home_screen.dart
+++ lib/view/guest_home_screen.dart
@@ -10,6 +10,7 @@ class GuestHomeScreen extends StatefulWidget {
 class _GuestHomeScreenState extends State<GuestHomeScreen> {
 
  final List<String> screenTitles = [
+   
    'Explore',
    'Saved',
    'Trips',
