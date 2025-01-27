c731abe7ecc7e33d3afd3252bb5c79afc5d1d27d
vengersmoon <vengersmon003@gmail.com>
2025-01-27 11:36:02 +0530

    d

---
+++ lib/view/guest_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GuestHomeScreen extends StatefulWidget {
+  const GuestHomeScreen({super.key});
+
+  @override
+  State<GuestHomeScreen> createState() => _GuestHomeScreenState();
+}
+
+class _GuestHomeScreenState extends State<GuestHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
