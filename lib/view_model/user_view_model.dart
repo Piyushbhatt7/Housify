3f98cf013a56f587eef936d3ee97944bde7de122
vengersmoon <vengersmon003@gmail.com>
2025-01-27 11:39:39 +0530

    g

--- lib/view_model/user_view_model.dart
+++ lib/view_model/user_view_model.dart
@@ -5,7 +5,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:housify/models/app_constants.dart';
-import 'package:housify/view/account_screen.dart';
+import 'package:housify/view/guestScreens/account_screen.dart';
 
 class UserViewModel {
 
