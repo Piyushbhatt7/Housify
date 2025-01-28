76a5b1211afebbb05116101183ecc04ef008458c
vengersmoon <vengersmon003@gmail.com>
2025-01-28 22:02:01 +0530

    h

--- lib/view/widgets/amenities_ui.dart
+++ lib/view/widgets/amenities_ui.dart
@@ -6,7 +6,7 @@ class AmenitiesUi extends StatefulWidget {
   int startValue;
   Function decreaseValue;
   Function increaseValue;
-  
+
  AmenitiesUi({super.key, required this.type, required this.startValue, required this.decreaseValue, required this.increaseValue});
 
   @override
@@ -14,6 +14,16 @@ class AmenitiesUi extends StatefulWidget {
 }
 
 class _AmenitiesUiState extends State<AmenitiesUi> {
+
+  int? _valueDigit;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _valueDigit = widget.startValue;
+  }
 
   @override
   Widget build(BuildContext context) {
