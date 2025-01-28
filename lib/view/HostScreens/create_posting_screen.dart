059a149bdb0282187b72e95a69ab8c79508c90d2
vengersmoon <vengersmon003@gmail.com>
2025-01-28 10:28:05 +0530

    f

---
+++ lib/view/HostScreens/create_posting_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CreatePostingScreen extends StatefulWidget {
+  const CreatePostingScreen({super.key});
+
+  @override
+  State<CreatePostingScreen> createState() => _CreatePostingScreenState();
+}
+
+class _CreatePostingScreenState extends State<CreatePostingScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Create / Update a Posting"
+        ),
+
+        actions: [
+          IconButton(onPressed: ()
+          {
+
+
+          }, icon: const Icon(Icons.upload)
+          )
+        ],
+      ),
+    );
+  }
+}
