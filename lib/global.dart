e1db991763bb6bc295270114783fa8e8c02f942d
vengersmoon <vengersmon003@gmail.com>
2025-01-30 21:11:07 +0530

    f

--- lib/global.dart
+++ lib/global.dart
@@ -1,3 +1,5 @@
+import 'package:housify/view_model/posting_view_model.dart';
 import 'package:housify/view_model/user_view_model.dart';
 
 UserViewModel userViewModel = UserViewModel();
+PostingViewModel 
