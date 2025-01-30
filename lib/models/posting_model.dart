e8a676aee1b414f4999982b1dd5e9ae406bbf8e1
vengersmoon <vengersmon003@gmail.com>
2025-01-30 22:57:56 +0530

    j

--- lib/models/posting_model.dart
+++ lib/models/posting_model.dart
@@ -22,7 +22,7 @@ class PostingModel {
   Map<String, int>? beds;
  Map<String, int>? bathrooms;
 
- PostingModel({this.id = "", this.name = "", this.type, this.price = 0, this.description = "", this.address = "", this.city = "", this.country = "", this.rating = ""})
+ PostingModel({this.id = "", this.name = "", this.type, this.price = 0, this.description = "", this.address = "", this.city = "", this.country = "", this.host = ""})
  {
    
  }
