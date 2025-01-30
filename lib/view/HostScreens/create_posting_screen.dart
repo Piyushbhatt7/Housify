fe8108c792e7e75d6352e480baf23792106c4b78
vengersmoon <vengersmon003@gmail.com>
2025-01-30 21:30:39 +0530

    g

--- lib/view/HostScreens/create_posting_screen.dart
+++ lib/view/HostScreens/create_posting_screen.dart
@@ -134,6 +134,10 @@ class _CreatePostingScreenState extends State<CreatePostingScreen> {
         actions: [
           IconButton(onPressed: ()
           {
+            if(!formKey.currentState.validate())
+            {
+              
+            }
              postingViewModel.saveOrUpdateListing();
           }, icon: const Icon(Icons.upload)
           )
