5ba0cfabccd5fa548f9d3d8309af6eb72e33d35d
vengersmoon <vengersmon003@gmail.com>
2025-01-31 10:49:55 +0530

    g

--- lib/models/app_constants.dart
+++ lib/models/app_constants.dart
@@ -1,8 +1,12 @@
+import 'package:housify/models/contact_model.dart';
 import 'package:housify/models/user_model.dart';
 class AppConstants {
   
   static UserModel currentUser = UserModel();
 
 
-  ContactModel = createContactFrom
+  ContactModel = createContactFromUserModel()
+  {
+    return ContactModel();
+  }
 } 
