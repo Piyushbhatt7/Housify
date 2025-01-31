879955774ec6e276159b1ffe720917261bd909e3
vengersmoon <vengersmon003@gmail.com>
2025-01-31 23:17:12 +0530

    g

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:housify/models/booking_model.dart';
 import 'package:housify/models/contact_model.dart';
 import 'package:housify/models/posting_model.dart';
 
@@ -15,6 +16,8 @@ class UserModel extends ContactModel{
     DocumentSnapshot? snapshot;
 
     List<PostingModel>? myPostings;
+    List<BookingModel>? bookings;
+    
     
    UserModel({
    
