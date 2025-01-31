bfa81148dae7f18e1f3e98614300440fe7f82f1b
vengersmoon <vengersmon003@gmail.com>
2025-01-31 11:06:46 +0530

    t

--- lib/view_model/posting_view_model.dart
+++ lib/view_model/posting_view_model.dart
@@ -1,7 +1,11 @@
+import 'package:housify/models/posting_model.dart';
+
 class PostingViewModel {
 
   addListingInfoToFirestore() async
   {
-    
+     PostingModel posting = PostingModel();
+
+     posting.setImagesNames();
   }
 }
