1e326a0f45e801380536d9504f53ee54259eeae4
vengersmoon <vengersmon003@gmail.com>
2025-02-03 11:38:46 +0530

    f

--- lib/view/view_posting_screen.dart
+++ lib/view/view_posting_screen.dart
@@ -5,13 +5,27 @@ class ViewPostingScreen extends StatefulWidget {
 
   PostingModel? posting;
 
-   ViewPostingScreen({super.key});
+   ViewPostingScreen({super.key, this.posting});
 
   @override
   State<ViewPostingScreen> createState() => _ViewPostingScreenState();
 }
 
 class _ViewPostingScreenState extends State<ViewPostingScreen> {
+
+  PostingModel? posting;
+
+  getRequiredInfo()
+  {
+    
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    widget.posting;
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
