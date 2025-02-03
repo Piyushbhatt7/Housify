7a778f93cc3fb1fdebf0fecb0cead7ce682d9a03
vengersmoon <vengersmon003@gmail.com>
2025-02-03 12:18:16 +0530

    t

--- lib/view/view_posting_screen.dart
+++ lib/view/view_posting_screen.dart
@@ -179,7 +179,28 @@ class _ViewPostingScreenState extends State<ViewPostingScreen> {
                               style: TextStyle(
                                 fontSize: 14,
                               ),
+                              maxLines: 5,
                             ),
+                           ),
+
+                           Column(
+                            children: [
+
+                              GestureDetector(
+                                onTap: () {
+                                  
+                                },
+                                child: CircleAvatar(
+
+                                  radius: MediaQuery.of(context).size.width / 12.5,
+                                  backgroundColor: Colors.black,
+                                  child: CircleAvatar(
+                                    backgroundImage: posting!.host!.displayImage,
+                                    radius: MediaQuery.of(context).size.width / 13,
+                                  ),
+                                ),
+                              )
+                            ],
                            )
                     ],
                   ),
