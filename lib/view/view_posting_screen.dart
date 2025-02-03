50637227ea8979d9c07ea294cc44f7f91912c9e6
vengersmoon <vengersmon003@gmail.com>
2025-02-03 11:36:56 +0530

    r

---
+++ lib/view/view_posting_screen.dart
@@ -0,0 +1,13 @@
+class ViewPostingScreen extends StatefulWidget {
+  const ViewPostingScreen({super.key});
+
+  @override
+  State<ViewPostingScreen> createState() => _ViewPostingScreenState();
+}
+
+class _ViewPostingScreenState extends State<ViewPostingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
