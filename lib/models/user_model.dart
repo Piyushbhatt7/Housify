3b97ed3d3d41fa0a1eba999d1fec0fd29118d82f
vengersmoon <vengersmon003@gmail.com>
2025-02-04 23:01:18 +0530

    m

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:housify/global.dart';
 import 'package:housify/models/booking_model.dart';
 import 'package:housify/models/contact_model.dart';
 import 'package:housify/models/posting_model.dart';
