21cb18cf881292eb8ee8a5f66d77cbfaa46e4faf
vengersmoon <vengersmon003@gmail.com>
2025-02-04 10:02:37 +0530

    g

---
+++ lib/view/widgets/posting_info_trial_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostingInfoTrialUi extends StatefulWidget {
+  const PostingInfoTrialUi({super.key});
+
+  @override
+  State<PostingInfoTrialUi> createState() => _PostingInfoTrialUiState();
+}
+
+class _PostingInfoTrialUiState extends State<PostingInfoTrialUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
