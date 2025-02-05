004c0aa5bb608547a9da664aa5d06e2d24a381cb
vengersmoon <vengersmon003@gmail.com>
2025-02-05 10:45:52 +0530

    t

--- lib/view/HostScreens/create_posting_screen.dart
+++ lib/view/HostScreens/create_posting_screen.dart
@@ -233,6 +233,7 @@ class _CreatePostingScreenState extends State<CreatePostingScreen> {
             }
            
           //R postingModel = PostingModel();
+          PostingModel postingModel = PostingModel();
 
           Get.to(HostHomeScreen(index: 1,)); 
 
