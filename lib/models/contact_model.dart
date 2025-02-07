b33fc706337ebaca1c0cddf69a6e036f6028a1e9
vengersmoon <vengersmon003@gmail.com>
2025-02-07 22:41:50 +0530

    u

--- lib/models/contact_model.dart
+++ lib/models/contact_model.dart
@@ -26,7 +26,7 @@ class ContactModel {
    }
      UserModel createUserFromContact() {
        
-       return UserModel( ggg
+       return UserModel( gggu
          
          id: id!,
          firstName: firstName!,
