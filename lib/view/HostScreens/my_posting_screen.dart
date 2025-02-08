745981cf00acd802f3b43ce743cc47744ed2e22e
vengersmoon <vengersmon003@gmail.com>
2025-02-08 23:13:35 +0530

    my posting or listing page

--- lib/view/HostScreens/my_posting_screen.dart
+++ lib/view/HostScreens/my_posting_screen.dart
@@ -15,48 +15,37 @@ class MyPostingScreen extends StatefulWidget {
 class _MyPostingScreenState extends State<MyPostingScreen> {
   @override
   Widget build(BuildContext context) {
-   
     return Padding(
       padding: EdgeInsets.only(top: 25),
       child: ListView.builder(
-        itemCount: AppConstants.currentUser.myPostings!.length + 1,
-        itemBuilder: (context, index)
-        {
-          
-          return  Padding(
-        padding: EdgeInsets.fromLTRB(26, 0, 26, 26),
-
-        child: InkResponse(
-
-          onTap: () {
-            Get.to(CreatePostingScreen(
-              posting: (index == AppConstants.currentUser.myPostings!.length) ? null : 
-              AppConstants.currentUser.myPostings![index],
-            ));
-          },
-
-          child: Container(
-            decoration: BoxDecoration(
-               border: Border.all(
-                color: Colors.grey,
-                width: 1.2,
-
-               ),        
-            ),
-            child: (index == AppConstants.currentUser.myPostings!.length)   
-            ? PostingTileButton()  
-            : PostingListingTileUi(posting: AppConstants.currentUser.myPostings![index]),
-         
-          ),
-        ),
-         );   
-        }
-      
-      ),
-      
-      
-       );
- 
- 
+          itemCount: AppConstants.currentUser.myPostings!.length + 1,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.fromLTRB(26, 0, 26, 26),
+              child: InkResponse(
+                onTap: () {
+                  Get.to(CreatePostingScreen(
+                    posting:
+                        (index == AppConstants.currentUser.myPostings!.length)
+                            ? null
+                            : AppConstants.currentUser.myPostings![index],
+                  ));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.2,
+                    ),
+                  ),
+                  child: (index == AppConstants.currentUser.myPostings!.length)
+                      ? PostingTileButton()
+                      : PostingListingTileUi(
+                          posting: AppConstants.currentUser.myPostings![index]),
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
