1595221a3c647e665689aa9c2db87201e2126ff5
vengersmoon <vengersmon003@gmail.com>
2025-02-08 23:16:39 +0530

    global file

--- lib/global.dart
+++ lib/global.dart
@@ -3,7 +3,5 @@ import 'package:housify/view_model/posting_view_model.dart';
 import 'package:housify/view_model/user_view_model.dart';
 
 PostingModel postingModel = PostingModel();
-
 UserViewModel userViewModel = UserViewModel();
-
 PostingViewModel postingViewModel = PostingViewModel();
