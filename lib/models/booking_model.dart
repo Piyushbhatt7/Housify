2416fb25929bb8cb9e4bf615b1cd0927b77dcec2
vengersmoon <vengersmon003@gmail.com>
2025-02-08 23:12:51 +0530

    book the property or etc

--- lib/models/booking_model.dart
+++ lib/models/booking_model.dart
@@ -1,12 +1,11 @@
 import 'package:housify/models/contact_model.dart';
 import 'package:housify/models/posting_model.dart';
 
-class BookingModel {  
-   
-   String id = ""; 
-   PostingModel? posting; 
-   ContactModel? contact; 
-   List<DateTime>? dates;                    
- 
-   BookingModel();   
-} 
+class BookingModel {
+  String id = "";
+  PostingModel? posting;
+  ContactModel? contact;
+  List<DateTime>? dates;
+
+  BookingModel();
+}
